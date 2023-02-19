5b6e92b5887c3cf1bb3a8f6b390f3cc097160e8c
Alan Melone <alanmelone@protonmail.com>
2023-02-19 16:44:48 +0100

    - try to implement the left panel

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { AppComponent } from "src/app/app.component";
+import { LeftPanelComponent } from "src/app/components/left-panel/left-panel.component";
 import { MessageComponent } from "src/app/components/message/message.component";
 import { PriceComponent } from "src/app/components/price/price.component";
 
@@ -16,6 +17,10 @@ const routes: Routes = [
       {
         path: 'message',
         component: MessageComponent
+      },
+      {
+        path: 'left-panel',
+        component: LeftPanelComponent
       }
     ]
   }
